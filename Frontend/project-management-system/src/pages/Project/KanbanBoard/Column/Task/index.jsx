a23eaa8ad321ed312eb98e85ad5251da5da1cace
aneeshKm <aneeshkmokashi@rediffmail.com>
2025-02-14 21:56:05 -0500

    added drag and drop functionality

--- Frontend/project-management-system/src/pages/Project/KanbanBoard/Column/Task/index.jsx
+++ Frontend/project-management-system/src/pages/Project/KanbanBoard/Column/Task/index.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import './style.css';
+import { useDraggable } from '@dnd-kit/core';
 
-const Task = ({title, description}) =>{
+const Task = ({ task }) => {
+    const { attributes, listeners, transform, setNodeRef } = useDraggable({          // useDraggable hook is used to create task draggable
+        id: task.id                                                                  // id is used to connect to DndContext
+    })
+
+    const style = transform ? {                                                      // hold the values of x and y coordinates and styling when dragging.
+        transform: `translate(${transform.x}px, ${transform.y}px)`                   // Checking first if we are having transform and if no, undefined.
+    } : undefined
     return (
-        <div className="cursor-grab rounded-lg bg-neutral-700 p-4 shadow-sm hover:shadow-md">
-            <h3 className="font-medium text-neutral-100">{title}</h3>
-            <p className="mt-2 text-sm text-neutral-100">{description}</p>
+        <div ref={setNodeRef}
+            {...listeners}
+            {...attributes}
+            className="cursor-grab rounded-lg bg-neutral-700 p-4 shadow-sm hover:shadow-md"
+            style={style}>
+            <h3 className="font-medium text-neutral-100">{task.title}</h3>
+            <p className="mt-2 text-sm text-neutral-100">{task.description}</p>
         </div>
     )
 }
--- Frontend/project-management-system/src/pages/Project/KanbanBoard/index.jsx
+++ Frontend/project-management-system/src/pages/Project/KanbanBoard/index.jsx
@@ -1,55 +1,82 @@
 import React, { useState } from 'react';
 import Column from './Column';
 import './style.css';
+import { DndContext } from '@dnd-kit/core';
 
 const COLUMNS = [
-  {id:'BACKLOG', title: 'backlog'},
-  {id:'SELECTED', title:'selected'},
-  {id:'INPROGRESS', title:'inprogress'},
-  {id:'DONE', title:'done'},
+  { id: 'BACKLOG', title: 'backlog' },
+  { id: 'SELECTED', title: 'selected' },
+  { id: 'INPROGRESS', title: 'inprogress' },
+  { id: 'DONE', title: 'done' },
 ];
 
 const INITIAL_TASK = [
-  {id:1,
-    title:'New Research',
-    desc:'New task is here',
-    status:'BACKLOG'
+  {
+    id: 1,
+    title: 'New Research',
+    desc: 'New task is here',
+    status: 'BACKLOG'
   },
-  {id:2,
-    title:'New Research 2',
-    desc:'New task is here',
-    status:'SELECTED'
+  {
+    id: 2,
+    title: 'New Research 2',
+    desc: 'New task is here',
+    status: 'SELECTED'
   },
-  {id:3,
-    title:'New Research 3',
-    desc:'New task is here',
-    status:'INPROGRESS'
+  {
+    id: 3,
+    title: 'New Research 3',
+    desc: 'New task is here',
+    status: 'INPROGRESS'
   },
-  {id:4,
-    title:'New Research 4',
-    desc:'New task is here',
-    status:'INPROGRESS'
+  {
+    id: 4,
+    title: 'New Research 4',
+    desc: 'New task is here',
+    status: 'INPROGRESS'
   },
-  {id:5,
-    title:'New Research 5',
-    desc:'New task is here',
-    status:'BACKLOG'
+  {
+    id: 5,
+    title: 'New Research 5',
+    desc: 'New task is here',
+    status: 'BACKLOG'
   },
 ]
 
-const KanbanBoard = () =>{
+const KanbanBoard = () => {
   const [tasks, setTasks] = useState(INITIAL_TASK);
 
-  return(
+  const handleDragEnd = (event) => {                             // event is DragEndEvent from dnd core
+    const { active, over } = event
+
+    if (!over) return                                           // if drage is not over then the function should not be executed
+
+    const taskId = active.id                                    // id of task that we will be dropping
+    const nesStatus = over.id                                   // It is of type Task['status'] basically a column id we are dropping over  
+
+    // now updating the task
+    setTasks(() => tasks.map((task) =>                          // so basically here we are checking whether the task is active one or not
+      task.id === taskId ? {                                    // and if it is active one, then we are assining a new status to it
+        ...task,
+        status: nesStatus
+      } :
+        task,
+    ))
+
+  }
+
+  return (
     <div className='p-4'>
       <div className='flex gap-8'>
-        {COLUMNS.map((column) =>{
-          return (<Column key ={column.id} 
-            title ={column.title} 
-            tasks = {tasks.filter(task => task.status === column.id)
-            }/>
+      <DndContext onDragEnd={handleDragEnd}>
+        {COLUMNS.map((column) => {
+          return (<Column key={column.id}
+            column={column}
+            tasks={tasks.filter(task => task.status === column.id)
+            } />
           );
         })}
+        </DndContext>
       </div>
     </div>
   )
